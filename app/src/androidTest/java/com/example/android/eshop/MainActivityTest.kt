44bb425f1782e0ef6dc94f937de3908a9ca90229
Timothy Mubiru <mubstimor@gmail.com>
2020-01-09 11:12:08 +0300

    - set up login framework - add tests

--- app/src/androidTest/java/com/example/android/eshop/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/android/eshop/MainActivityTest.kt
@@ -1,6 +1,14 @@
 package com.example.android.eshop
 
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
@@ -13,6 +21,9 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import androidx.test.rule.ActivityTestRule
+import org.junit.Before
+import org.mockito.Mockito
+import org.mockito.Mockito.verify
 
 
 @RunWith(AndroidJUnit4::class)
@@ -23,10 +34,33 @@ class MainActivityTest {
     var activityRule: ActivityTestRule<MainActivity>
             = ActivityTestRule(MainActivity::class.java)
 
+    @Before
+    fun setUp() {
+        // Given on the home screen
+        val scenario = launchFragmentInContainer<LoginFragment>(Bundle(), R.style.Theme_Eshop)
+        val navController = Mockito.mock(NavController::class.java)
+        scenario.onFragment {
+            Navigation.setViewNavController(it.view!!, navController)
+        }
+
+    }
+
     @Test
-    fun test_display_helloWorld() {
-        onView(withId(R.id.helloWorld))
-            .check(matches(withText("Hello World!")))
+    fun splashScreen_loadsLoginFragment() {
+
+        // When - App name exists
+        onView(withId(R.id.name_label)).check(matches(withText("ESHOP")))
+    }
+
+    @Test
+    fun clickNextButton_navigateToProductGridFragment(){
+
+        onView(withId(R.id.username_edit_text)).perform(typeText("john"), closeSoftKeyboard())
+
+        onView(withId(R.id.password_edit_text)).perform(typeText("johndoe123"), closeSoftKeyboard())
+
+        // THEN  - Verify that we navigate to the add screen
+        // -- TBD
     }
 
 }
---
+++ app/src/main/java/com/example/android/eshop/LoginFragment.kt
@@ -0,0 +1,52 @@
+package com.example.android.eshop
+
+import android.os.Bundle
+import android.text.Editable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.login_fragment.*
+import kotlinx.android.synthetic.main.login_fragment.view.*
+
+/**
+ * Fragment representing the login screen for Shrine.
+ */
+
+const val PASSWORD_MINLENGTH = 8
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.login_fragment, container, false)
+
+        // Set an error if the password is less than 8 characters.
+        view.next_button.setOnClickListener({
+            if (!isPasswordValid(password_edit_text.text!!)) {
+                password_text_input.error = getString(R.string.es_error_password)
+            } else {
+                // Clear the error.
+                password_text_input.error = null
+                // Navigate to the next Fragment.
+                (activity as NavigationHost).navigateTo(ProductGridFragment(), false)
+            }
+        })
+
+        // Clear the error once more than 8 characters are typed.
+        view.password_edit_text.setOnKeyListener({ _, _, _ ->
+            if (isPasswordValid(password_edit_text.text!!)) {
+                // Clear the error.
+                password_text_input.error = null
+            }
+            false
+        })
+
+        return view
+    }
+
+    private fun isPasswordValid(text: Editable?): Boolean {
+        return text != null && text.length >= PASSWORD_MINLENGTH
+    }
+}
